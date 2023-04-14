421f143021249dcbe1c7b3e94a65d4bc1fddbdef
guoweisheng <guoweisheng@mindfly.tv>
2023-04-14 18:02:06 +0800

    fix: modify

---
+++ dist/static/js/index-0361f776.js
@@ -0,0 +1,1 @@
+import{s as t}from"./Dialog-84d71f57.js";import{a9 as a}from"./index-7338b424.js";const i=a(t);export{i as D};
--- dist/static/js/index-9cd32ae9.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./Dialog-4af56cf7.js";import{a9 as a}from"./index-1c2e86ec.js";const i=a(t);export{i as D};
